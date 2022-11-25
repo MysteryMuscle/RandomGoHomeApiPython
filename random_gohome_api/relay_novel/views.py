ba5d30ac32bc795264a2f337fb9f794a67907b49
pjc1991 <whdekf@gmail.com>
2022-11-25 16:10:22 +0900

    feat : add relay novel model

--- random_gohome_api/relay_novel/views.py
+++ random_gohome_api/relay_novel/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from relay_novel.models import relay_novel
+from relay_novel.serializers import relay_novelSerializer
 
 # Create your views here.
+
+class relay_novel_list(APIView):
+    
+    def get(self, request):
+        relay_novel1 = relay_novel.objects.all()
+        serializer = relay_novelSerializer(relay_novel1, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
